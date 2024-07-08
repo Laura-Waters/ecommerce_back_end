d43188820893e1e6aa48401768c5012f814854d5
Laura <laurawatersalston@gmail.com>
2024-07-08 19:41:03 -0400

    index.js model

--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category   
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', 
+  onDelete: 'CASCADE',
+}); 
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) 
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag)   
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+});
 
 module.exports = {
   Product,
@@ -18,3 +31,6 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+
